1059174fc06c2646df048531eb863413d2269d27
Immanuel Albrecht <github@genetic-superior.de>
2011-02-24 14:43:31 +0100

    primitive swing oscillator

--- DrumsEmulation/src/drumsemulation/snd/swingOscillator.java
+++ DrumsEmulation/src/drumsemulation/snd/swingOscillator.java
@@ -9,15 +9,16 @@ package drumsemulation.snd;
  * @author immanuel
  */
 public class swingOscillator extends hitGenerator {
-    //all variables are fixed point integers in 8.23 binary format, mass is renormalized to 1
+    
+    int frequency; // frequency in Hz
+    long amplitude31; //amplitude shifted by 31
+    long damping31; //damping factor shifted by 31
+    long phase_frame; // start sample time
 
-    long x23; //current position
-    long v23; //current velocity
-    long k23; //spring force factor
-    long mu23; //friction factor
-    long one_by_dt31; //multiplicative inverse of length of a timestep
     long[] c_lvl31; //channel panning levels
     long[] chan_lvls;
+
+    static functionTables ft = functionTables.getObject();
 
     public swingOscillator(String description) {
         this();
@@ -27,14 +28,13 @@ public class swingOscillator extends hitGenerator {
     public swingOscillator() {
         super();
         description = "swOsc()";
-        x23 = 0;
-        v23 = 0;
-        k23 = 1000 << 23;
-        mu23 = 0;
+        phase_frame = 0;
+        frequency = 100;
+        amplitude31 = 1l<<31;
         c_lvl31 = new long[]{1l << 31, 1l << 31};
-        chan_lvls = new long[]{1l << 31, 1l << 31};
-        int sample_rate = drumsemulation.DrumsEmulationApp.getApplication().getSampleRate();
-        one_by_dt31 = (1l << 31) / ((long) sample_rate);
+        chan_lvls = new long[]{1l << 31, 1l << 31};        
+        damping31 = (9999l << 31)/10000l;
+        
     }
 
     @Override
@@ -44,33 +44,32 @@ public class swingOscillator extends hitGenerator {
             chan_lvls[c] = (c_lvl31[c] * lvl31) >> 31;
         }
         long next_frame_block = start_frame + frames;
-
+        long relative_frame = start_frame - phase_frame;
         int idx = 0;
         synchronized (sync_token) {
             for (long f = start_frame; f < next_frame_block; ++f) {
-                long F23 = -((k23 * x23) >> 23);
-                v23 += ((F23 - ((v23 * mu23) >> 23)) * one_by_dt31) >> 31; //F = m*dv/dt = 1*dv/dt
-                long old_x23 = x23;
-                x23 += (v23 * one_by_dt31) >> 31;
-
-                if ((old_x23 > 0) && (x23<=0)) {
-                    System.out.println(f);
+                long position = (ft.cosine(relative_frame, frequency)*amplitude31)>>31;
+                long damped31 = (amplitude31*damping31)>>31;
+                if (damped31 < 0) {
+                    System.out.println(amplitude31+"->"+damped31);
                 }
+                amplitude31 = damped31;
 
                 for (int c = 0; c < channels; ++c) {
-                    buffer[idx] += (int)((x23 * chan_lvls[c]) >> 23);
+                    buffer[idx] += (position * chan_lvls[c]) >> 31;
                     idx++;
                 }
+                relative_frame++;
             }
         }
     }
 
     @Override
     public void hit(long when, int level) {
-        System.out.println(x23);
+        
         synchronized (sync_token) {
-            x23 = 1<<23;
-            v23 = 0;
+            amplitude31 = ((long)(level & 0x7F))<<23;
+            phase_frame = when;
         }
     }
 
@@ -79,12 +78,12 @@ public class swingOscillator extends hitGenerator {
 
         System.out.println("1l<<31 = "+(1l<<31));
 
-        System.out.println("1/dt="+s.one_by_dt31);
+        System.out.println("(1*1l)<<31 = "+((1*1l)<<31));
 
-        s.hit(0,0);
+        s.hit(0,127);
         int[] buffer = new int[] {0};
         for (long f=0;f<100;++f) {
-            System.out.println(f+" = "+new Float(buffer[0])/new Float(1l<<31)+"  x="+s.x23+"  v="+s.v23);
+            System.out.println(f+" = "+new Float(buffer[0])/new Float(1l<<31));
             buffer[0] = 0;
             s.additiveSynthesis(f, buffer, 1,1, 1l<<31);
      
@@ -100,7 +99,7 @@ public class swingOscillator extends hitGenerator {
         }
 
         for (long f=28087;f<28100;++f) {
-            System.out.println(f+" = "+new Float(buffer[0])/new Float(1l<<31)+"  x="+s.x23+"  v="+s.v23);
+            System.out.println(f+" = "+new Float(buffer[0])/new Float(1l<<31));
             buffer[0] = 0;
             s.additiveSynthesis(f, buffer, 1,1, 1l<<31);
 
