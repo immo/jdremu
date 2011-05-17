5b4ebda09b08081e69d9195dd0bd5a8c93a912f1
Immanuel Albrecht <github@genetic-superior.de>
2011-05-17 10:33:57 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ -n $(readlink $0) ]
+echo $(readlink $0)
+if [ -h "$0" ];
 then
-cd $(dirname $0)/DrumsEmulation/jars
-else
 cd $(dirname $(readlink $0))/DrumsEmulation/jars
+else
+cd $(dirname $0)/DrumsEmulation/jars
 fi
 java -jar DrumsEmulation-0.0.0.0a.jar
 
